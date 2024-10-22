15b014fd5300fbc412add5a01d7d171eea594aa6
Boleslaw Wroblewski <boleslaw.wroblewski@intel.com>
2024-10-22 09:07:12 +0200

    stderr i stdout rozdzielone prawidlowo tak aby symulowac git lfs pull index error

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 test-mock:
 	go run main.go > stdout.txt 2> stderr.txt
+build:
+	go build -o mock-stderr main.go
--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 
 func main() {
 	// Define the text to be printed to stderr
-	text1 := "Error updating the Git index:"
-	text2 := "jakis blad"
+	text1 := "jakis tekst w stdout"
+	text2 := "Error updating the Git index:"
 
 	// Print the text to stdout using fmt.Fprintf
 	fmt.Fprintf(os.Stdout, "%s\n", text1)
